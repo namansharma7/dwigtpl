ca944db85ea933eb49d19f759b565c15f3950aa4
DESKTOP-LE8AKLL\Lenovo <namansharmag7@gmail.com>
2024-02-07 21:55:26 +0530

    - Adding index file

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+
+const userRoute = require('./routes/userRoutes');
+
+const port = process.env.SERVER_PORT | 4001;
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views','./views');
+app.use(express.static("public"));
+
+app.use('/user', userRoute);
+
+app.listen(port, function(){
+    console.log(`app is running on port : ${port}`);
+});
